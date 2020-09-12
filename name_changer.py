e90360889701b5468d96c91d109215d0e8e4259c
Abhishek Raha <dsklegend@live.com>
2020-09-12 14:40:11 +0530

    Fixed bug

--- name_changer.py
+++ name_changer.py
@@ -1,6 +1,7 @@
 import os
 
 want_to_continue = False
+is_name_string_created = False
 
 dir_list = os.listdir('.')
 for file in dir_list : 
@@ -11,12 +12,13 @@ for file in dir_list :
         org_name = file
         file = file.split(" ")
         index = 0
-        for words in file:
-            print("[" + str(index) +"] - " + str(words))
-            index = index + 1
-        anime_index = raw_input("Enter the index position : ")
-        anime_name_from = raw_input("Enter the index name starts from : ")
-        anime_name_to = raw_input("Enter the index name ends at : ")
+        if is_name_string_created == False:
+            for words in file:
+                print("[" + str(index) +"] - " + str(words))
+                index = index + 1
+            anime_index = raw_input("Enter the index position : ")
+            anime_name_from = raw_input("Enter the index name starts from : ")
+            anime_name_to = raw_input("Enter the index name ends at : ")
         # print(file)
         #   Name Format : <Ep. No> <Anime Name>
         new_name = new_name + ''.join(file[int(anime_index)].split(']')[0]) + ". " + ' '.join(file[int(anime_name_from):int(anime_name_to)+1]) +"."+ ''.join(file[-1].split('.')[-1])
@@ -25,8 +27,9 @@ for file in dir_list :
             ok = raw_input("Is the file name ok ? [yes/no] : ")
             if ok == "yes":
                 want_to_continue = True
+                is_name_string_created = True
             else:
                 print("Exiting . . .")
                 exit(1)
         
-        #os.rename(org_name,new_name)    
+        os.rename(org_name,new_name)    
