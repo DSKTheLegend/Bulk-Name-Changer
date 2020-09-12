bb79bb38b71fdf8e321e1b0e14e5203cc77af195
Abhishek Raha <dsklegend@live.com>
2020-09-12 14:33:18 +0530

    Fixed need to manually edit the index position everytime

--- name_changer.py
+++ name_changer.py
@@ -10,9 +10,16 @@ for file in dir_list :
     if file != 'name_changer.py':
         org_name = file
         file = file.split(" ")
+        index = 0
+        for words in file:
+            print("[" + str(index) +"] - " + str(words))
+            index = index + 1
+        anime_index = raw_input("Enter the index position : ")
+        anime_name_from = raw_input("Enter the index name starts from : ")
+        anime_name_to = raw_input("Enter the index name ends at : ")
         # print(file)
         #   Name Format : <Ep. No> <Anime Name>
-        new_name = new_name + ''.join(file[9].split(']')[0]) + ". " + ' '.join(file[1:8]) +"."+ ''.join(file[-1].split('.')[-1])
+        new_name = new_name + ''.join(file[int(anime_index)].split(']')[0]) + ". " + ' '.join(file[int(anime_name_from):int(anime_name_to)+1]) +"."+ ''.join(file[-1].split('.')[-1])
         print(new_name)
         if want_to_continue == False : 
             ok = raw_input("Is the file name ok ? [yes/no] : ")
@@ -20,6 +27,6 @@ for file in dir_list :
                 want_to_continue = True
             else:
                 print("Exiting . . .")
-                os.exit(1)
+                exit(1)
         
-        os.rename(org_name,new_name)    
+        #os.rename(org_name,new_name)    
